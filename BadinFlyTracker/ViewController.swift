7d330943ebdd8cde58d663c988a5f8a6fc18c240
Dominic <18anthonyd@badinhs.org>
2017-01-05 08:27:56 -0500

    Segues added

--- BadinFlyTracker/ViewController.swift
+++ BadinFlyTracker/ViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var FlyTable: UITableView!
+    @IBAction func AddButton(_ sender: Any) {
+        performSegue(withIdentifier: "AddSegue", sender: "🤑")
+    }
+     var fly = ["Fly 1- Room 114", "Fly 2- Room 107", "Fly 3- Room 107", "Fly 4- Room 107", "Fly 5- Room 105", "Fly 6- Room 005", "Fly 7- Room 105", "Fly 8- Room 114"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        FlyTable.dataSource = self
+        
+        FlyTable.delegate = self
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fly.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = fly[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "TableSegue", sender: "🐋")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+   
 }
-
